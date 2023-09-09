10354d186efa7bafcd0dca13d992361bc51e0163
ccordova <ccordova78@cnm.edu>
2023-09-08 21:47:35 -0600

    initial commit

---
+++ msAlias.ps1
@@ -0,0 +1,64 @@
+<#
+    Author: Corban C.
+    Date modified: 2023-09-08
+
+    #ToDo: Fix delete menu item
+#>
+# Prompt user for username
+$username = Read-Host "Please enter your username (e.g., user@example.com)"
+
+# Connect to Exchange Online without showing progress
+Connect-ExchangeOnline -UserPrincipalName $username -ShowProgress $false
+
+# Get the user's email aliases (proxy addresses)
+$user = Get-Mailbox -Identity $username
+$aliases = ($user.EmailAddresses | Select-String -Pattern "smtp")
+If($aliases -eq "")
+{
+ $aliases="-"
+}
+Write-Host "Current Aliases:"
+$aliases
+
+# Loop until a valid choice is made
+while ($true) {
+    # Ask the user what they want to do
+    $action = Read-Host "Would you like to (1) create an alias, (2) delete an alias, or (3) exit? Enter 1, 2, or 3"
+
+    # Create an alias
+    if ($action -eq "1") {
+        $newAlias = Read-Host "Enter the new alias you would like to create (e.g., alias@example.com)"
+        #$aliases += $newAlias
+        Set-Mailbox $username -EmailAddresses @{Add=$newAlias}
+        Write-Host "Alias created."
+        #break
+    }
+
+    # Delete an alias
+    elseif ($action -eq "2") {
+        $deleteAlias = Read-Host "Enter the alias you would like to delete"
+        if ($aliases -contains $deleteAlias) {
+            $aliases = $aliases | Where-Object { $_ -ne $deleteAlias }
+            Set-Mailbox -Identity $username -ProxyAddresses $aliases
+            Write-Host "Alias deleted."
+            #break
+        }
+        else {
+            Write-Host "Alias not found."
+        }
+    }
+
+    # Exit
+    elseif ($action -eq "3") {
+        Write-Host "Exiting."
+        break
+    }
+
+    # Invalid choice
+    else {
+        Write-Host "Invalid choice. Please try again."
+    }
+}
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false
